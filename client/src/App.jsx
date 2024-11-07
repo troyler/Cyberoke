f73915940b4f59823a3e852a8af78f4de18d2af4
troyler <tmreinert22@gmail.com>
2024-11-07 11:21:07 -0500

    profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import About from "./pages/About";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -14,8 +15,10 @@ export default function App() {
           <Route path = "/" element = {<Home />}/> 
           <Route path = "/about" element = {<About />}/> 
           <Route path = "/sign-in" element = {<SignIn />}/> 
-          <Route path = "/sign-up" element = {<SignUp />}/> 
-          <Route path = "/profile" element = {<Profile />}/> 
+          <Route path = "/sign-up" element = {<SignUp />}/>
+          <Route  element = {<PrivateRoute />}>
+            <Route path = "/profile" element = {<Profile />}/> 
+          </Route>
         </Routes>
     </BrowserRouter>
   )
